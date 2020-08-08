664527d034f407d54dd2cfa75e4fd2b85efae6e8
Sekator778 <sekator778@gmail.com>
2020-08-08 15:16:59 +0300

    migration up

--- src/main/java/com/example/sweater/model/User.java
+++ src/main/java/com/example/sweater/model/User.java
@@ -17,6 +17,9 @@ public class User implements UserDetails {
     private String password;
     private boolean active;
 
+    private String email;
+    private String activationCode;
+
     @ElementCollection(targetClass = Role.class, fetch = FetchType.EAGER)
     @CollectionTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"))
     @Enumerated(EnumType.STRING)
@@ -87,5 +90,21 @@ public class User implements UserDetails {
 
     public String getUsername() {
         return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getActivationCode() {
+        return activationCode;
+    }
+
+    public void setActivationCode(String activationCode) {
+        this.activationCode = activationCode;
     }
 }
